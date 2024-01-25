a762fc8bd79b65b87a5a0ef95bce99362a27d8b7
mk3709 <mk3709@srmist.edu.in>
2024-01-25 11:28:16 +0530

    Stake_added

--- src/com/bridgelabz/gamblingstimulation/gamblingstimulation.java
+++ src/com/bridgelabz/gamblingstimulation/gamblingstimulation.java
@@ -2,5 +2,15 @@ package com.bridgelabz.gamblingstimulation;
 
 public class gamblingstimulation {
 
+    public static  void main(String[] args) {
 
+        int stake = 100;
+
+        int bet = 1;
+
+        int every_day_stake = stake-bet;
+
+
+        System.out.println(every_day_stake);
+    }
 }
