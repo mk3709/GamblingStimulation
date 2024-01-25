9f05ec0e8baa82fa283e2a03b2c83db20471f9b1
mk3709 <mk3709@srmist.edu.in>
2024-01-25 12:02:50 +0530

    total_amount_added

--- src/com/bridgelabz/gamblingstimulation/gamblingstimulation.java
+++ src/com/bridgelabz/gamblingstimulation/gamblingstimulation.java
@@ -1,35 +1,54 @@
 package com.bridgelabz.gamblingstimulation;
 
 
+class Win_or_Loose{
+
+public int getEverydayStake() {
+    Scanner scanner = new Scanner(Systm.in);
+    String n = new.nexLine();
+
+    if (n == 'won' || n == 'loose') {
+        every_day_stake = every_day_stake * 50 / 100;
+    }
+
+retrun every_day_stake;
+}
+}
 public class gamblingstimulation {
 
     public static  void main(String[] args) {
         int stake = 100;
         int bet = 1;
-        int every_day_stake = stake-bet;
+         int every_day_stake = stake-bet;
         System.out.println("enter the Bet is won or not");
-        Scanner scanner = new Scanner(Systm.in);
-        String n=new.nexLine();
 
-        if(n=='won'||n=='loose')
+       gamblingstimulation obj= new gamblingstimulation();
+           obj.getEverydayStake();
+
+      int total_amount;
+
+
+        for(int i=0;i<100;i++)
         {
-            every_day_stake=every_day_stake*50/100;
+            if(i>20) {
+                total_amount -= every_day_stake;
+            }
+
+            if(n==win)
+            {
+                total_amount++;
+            }
+            else
+            {
+                total_amount---;
+            }
         }
 
-        System.out.println(every_day_stake);
-        if(bet > 1)
-        {
-          bet++;
-        }
-        else
-        {
-            bet--;
-
-        }
 
 
 
 
-        System.out.println(every_day_stake);
+
+        System.out.println(total_amount);
     }
 }
