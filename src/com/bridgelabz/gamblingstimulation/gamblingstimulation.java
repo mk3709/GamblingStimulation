3995ed182ddff243557d102cc65f7984a41b0276
mk3709 <mk3709@srmist.edu.in>
2024-01-25 11:47:29 +0530

    win_or_loose_stake_for_every_day

--- src/com/bridgelabz/gamblingstimulation/gamblingstimulation.java
+++ src/com/bridgelabz/gamblingstimulation/gamblingstimulation.java
@@ -4,15 +4,19 @@ package com.bridgelabz.gamblingstimulation;
 public class gamblingstimulation {
 
     public static  void main(String[] args) {
-
         int stake = 100;
-
         int bet = 1;
-
         int every_day_stake = stake-bet;
+        System.out.println("enter the Bet is won or not");
+        Scanner scanner = new Scanner(Systm.in);
+        String n=new.nexLine();
 
+        if(n=='won'||n=='loose')
+        {
+            every_day_stake=every_day_stake*50/100;
+        }
 
-
+        System.out.println(every_day_stake);
         if(bet > 1)
         {
           bet++;
